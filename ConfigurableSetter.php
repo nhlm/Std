6114198c7e35f3ea206e371496a46eea17f3aa9d
Payam Naderi <naderi.payam@gmail.com>
2016-04-29 16:45:20 +0430

    code cleanup

--- ConfigurableSetter.php
+++ ConfigurableSetter.php
@@ -1,7 +1,7 @@
 <?php
 namespace Poirot\Std;
 
-// DO_LEAST_PHPVER_SUPPORT
+// DO_LEAST_PHPVER_SUPPORT 5.4 traits
 if (version_compare(phpversion(), '5.4.0') < 0) {
     ## php version not support traits
     require_once __DIR__.'/fixes/ConfigurableSetter.php';
